eca838b0e2ed0580018a75392c04272499d37ab5
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-11-18 11:51:35 -0700

    make chuck norris joke app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [joke, setJoke] = useState({})
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-API-Key': 'Pz5s/PcmYDE3PqSzPpfvNA==Ome2rxMJlOQOmk81',
+        
+      }
+    };
+    
+    fetch('https://api.api-ninjas.com/v1/chucknorris?', options)
+      .then(response => response.json())
+	.then(response => setJoke(response))
+	.catch(err => console.error(err));
+  }, [])
+
+const newJoke = (event) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-API-Key': 'Pz5s/PcmYDE3PqSzPpfvNA==Ome2rxMJlOQOmk81',
+      
+    }
+  };
+  
+  fetch('https://api.api-ninjas.com/v1/chucknorris?', options)
+    .then(response => response.json())
+    .then(response => setJoke(response))
+    .catch(err => console.error(err));
+}  
+
+return (
+  <div className='App'>
+    <h3 className='title'>chuckdogz</h3>
+    <div className='riddle-container'>
+    <h2>{joke.joke}</h2>
+    
+    <button className='button-17' onClick={newJoke}>GIMME ANOTHER!</button>
     </div>
-  );
+    </div>
+)
 }
 
 export default App;
